c3b40e3d77277afa629ea9203873bb9ceef01485
Sharin Bi <104842811@student.swin.edu.au>
2024-10-08 10:25:46 +0800

    update 3.2

--- docs/lab_work/Lab3.2/lab3.2.js
+++ docs/lab_work/Lab3.2/lab3.2.js
@@ -1,10 +1,8 @@
 // Set the width and height of the SVG
-var w = 650;
-var h = 500;
-
+var w = 1000;
+var h = 1000;
 // Set padding to create margins
 var padding = 100;
-
 // Define the dataset: each subarray contains [x, y, z] values
 var dataset = [
     [40, 20, 5],
@@ -18,22 +16,29 @@ var dataset = [
     [85, 21, 13],
     [220, 88, 9]
 ];
-
 // Create x-scale
 var xScale = d3.scaleLinear()
     .domain([d3.min(dataset, function (d) { return d[0]; }), // Min x value
     d3.max(dataset, function (d) { return d[0]; })]) // Max x value
     .range([padding, w - padding]); // Output range
-
 // Create y-scale
 var yScale = d3.scaleLinear()
     .domain([d3.min(dataset, function (d) { return d[1]; }), // Min y value
     d3.max(dataset, function (d) { return d[1]; })]) // Max y value
-    .range([padding, h - padding]); // Output range (note: this is flipped for SVG)
+    .range([h - padding, padding]); // Output range (flipped for SVG)
+
+// Create x-axis
+var xAxis = d3.axisBottom()
+    .scale(xScale)
+    .ticks(5);
+
+// Create y-axis
+var yAxis = d3.axisLeft()
+    .scale(yScale)
+    .ticks(5);
 
 // Create SVG element
 var svg = d3.select("body")
-    .data(dataset)
     .append("svg")
     .attr("width", w)
     .attr("height", h)
@@ -45,7 +50,7 @@ svg.selectAll("circle")
     .enter()
     .append("circle")
     .attr("cx", function (d, i) { return xScale(d[0]); }) // x-coordinate
-    .attr("cy", function (d) { return yScale(d[1]); })    // y-coordinate
+    .attr("cy", function (d) { return yScale(d[1]); }) // y-coordinate
     .attr("r", 5) // radius
     .attr("fill", function (d) {
         // Color the circle red if x-value is 250, otherwise yellow
@@ -62,7 +67,33 @@ svg.selectAll("text")
     .enter()
     .append("text")
     .attr("x", function (d) { return xScale(d[0]); }) // x-position of text
-    .attr("y", function (d) { return yScale(d[1]); }) // y-position of text
+    .attr("y", function (d) { return yScale(d[1]) - 10; }) // y-position of text, slightly above the point
     .style("font-style", "italic")
-    .style("fill", d3.color("white"))
+    .style("fill", "white") // Changed from white to black for better visibility
+    .style("font-size", "12px") // Added for better readability
     .text(function (d) { return d[0] + ", " + d[1]; }); // Text content
+
+// Add x-axis
+svg.append("g")
+    .attr("transform", "translate(0," + (h - padding) + ")")
+    .call(xAxis);
+
+// Add y-axis
+svg.append("g")
+    .attr("transform", "translate(" + padding + ",0)")
+    .call(yAxis);
+
+// Add x-axis label
+svg.append("text")
+    .attr("transform", "translate(" + (w / 2) + " ," + (h - padding / 3) + ")")
+    .style("text-anchor", "middle")
+    .text("X-axis");
+
+// Add y-axis label
+svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", padding / 2)
+    .attr("x", 0 - (h / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Y-axis");
